b7d10ab1d851094b5d3411c850747efaeb98f0d3
mcapelle <capelle.mikael@gmail.com>
2015-08-24 13:49:03 +0200

    Create first test file for BootstrapFormHelper.

--- tests/TestCase/View/Helper/BootstrapFormHelperTest.php
+++ tests/TestCase/View/Helper/BootstrapFormHelperTest.php
@@ -7,37 +7,37 @@ use Cake\TestSuite\TestCase;
 use Cake\View\View;
 
 class BootstrapFormHelperTest extends TestCase {
-	
-	/**
- 	 *
-	 */
-	public $View ;
-	public $Form ;
-	
+
+    /**
+     *
+     */
+    public $View ;
+    public $Form ;
+
     public function setUp() {
-		parent::setUp();
-		$this->View = new View();
-		$this->Form = new BootstrapFormHelper ($this->View);
+        parent::setUp();
+        $this->View = new View();
+        $this->Form = new BootstrapFormHelper ($this->View);
     }
 
     public function testCreate () {
 
     }
-	
-	public function testInputText () {
+
+    public function testInputText () {
 
     }
-	
-	public function testInputSelect () {
-		
-	}
-	
-	public function testInputRadio () {
-		
-	}
-	
-	public function testInputCheckbox () {
-		
-	}
-	
+    
+    public function testInputSelect () {
+
+    }
+
+    public function testInputRadio () {
+
+    }
+
+    public function testInputCheckbox () {
+
+    }
+
 }
