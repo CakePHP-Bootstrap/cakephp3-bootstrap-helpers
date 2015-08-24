180ddc32c06f938d9ea74691d92b750b28b92335
Mikaël Capelle <capelle.mikael@gmail.com>
2015-08-24 20:20:58 +0200

    Create TestCase for BootstrapStringTemplate. Correct error in BootstrapStringTemplate.
    BootstrapStringTemplate does not output duplicate "class" attribute.

---
+++ tests/TestCase/View/BootstrapStringTemplateTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Bootstrap\Test\TestCase\View;
+
+use Bootstrap\View\BootstrapStringTemplate;
+use Cake\TestSuite\TestCase;
+use Cake\View\View;
+
+class BootstrapStringTemplateTest extends TestCase {
+
+    public function setUp () {
+        parent::setUp();
+        $this->templater = new BootstrapStringTemplate () ;
+    }
+
+    public function test () {
+        $this->templater->add([
+            'test_default' => '<p{{attrs}}>{{content}}</p>',
+            'test_attrs_class' => '<p class="test-class {{attrs.class}}"{{attrs}}>{{content}}</p>'
+        ]) ;
+        // Standard test
+        $result = $this->templater->format ('test_default', [
+            'attrs' => ' id="test-id" class="test-class"',
+            'content' => 'Hello World!'
+        ]);
+        $this->assertHtml([
+            ['p' => [
+                'id' => 'test-id',
+                'class' => 'test-class'
+            ]],
+            'Hello World!',
+            '/p'
+        ], $result) ;
+        // Test with class test
+        $result = $this->templater->format ('test_attrs_class', [
+            'attrs' => ' id="test-id" class="test-class-2"',
+            'content' => 'Hello World!'
+        ]);
+        $this->assertHtml([
+            ['p' => [
+                'id' => 'test-id',
+                'class' => 'test-class test-class-2'
+            ]],
+            'Hello World!',
+            '/p'
+        ], $result) ;
+        // Test with class test
+        $result = $this->templater->format ('test_attrs_class', [
+            'attrs' => 'class="test-class-2" id="test-id"',
+            'content' => 'Hello World!'
+        ]);
+        $this->assertHtml([
+            ['p' => [
+                'id' => 'test-id',
+                'class' => 'test-class test-class-2'
+            ]],
+            'Hello World!',
+            '/p'
+        ], $result) ;
+    }
+
+} 
