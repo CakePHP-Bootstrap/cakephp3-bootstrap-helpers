e7ff4ab103462e808c3320b2b2e9dd30fb3f04fc
Holt59 <capelle.mikael@gmail.com>
2016-02-18 19:17:52 +0100

    Clean code and doc.

--- src/View/Helper/BootstrapPanelHelper.php
+++ src/View/Helper/BootstrapPanelHelper.php
@@ -31,17 +31,17 @@ class BootstrapPanelHelper extends Helper {
     public $helpers = ['Html'];
 
     public $current = NULL ;
-    
+
     /**
-     * 
-     * Create a Twitter Bootstrap like panel. 
+     *
+     * Create a Twitter Bootstrap like panel.
      *
      * @param array|string $title If array, works as $options, otherwize used as the panel title.
-     * @param array $options Options for the main div of the panel. 
+     * @param array $options Options for the main div of the panel.
      *
      * Extra options (useless if $title not specified) :
      *     - no-body: Do not open the body after the create (default false)
-    **/
+     **/
     public function create($title = null, $options = []) {
 
         if (is_array($title)) {
@@ -52,12 +52,12 @@ class BootstrapPanelHelper extends Helper {
         unset ($options['no-body']);
         $type   = $this->_extractOption('type', $options, 'default');
         unset ($options['type']);
-        
+
         $options = $this->addClass($options, ['panel', 'panel-'.$type]);
         $class   = $options['class'];
         unset ($options['class']);
-        
-	    $res = $this->Html->div($class, null, $options);
+
+        $res = $this->Html->div($class, null, $options);
         if (is_string($title) && $title) {
             $res .= $this->_createHeader($title, []) ;
             if (!$nobody) {
@@ -65,16 +65,17 @@ class BootstrapPanelHelper extends Helper {
             }
         }
         return $res ;
-	}
-    	
+    }
+
     /**
-     * 
-     * End a panel. If $title is not null, the ModalHelper::footer functions is called with $title and $options arguments.
+     *
+     * End a panel. If $title is not null, the ModalHelper::footer functions
+     * is called with $title and $options arguments.
      *
      * @param string|null $buttons
      * @param array $options
-     * 
-    **/
+     *
+     **/
     public function end ($title = null, $options = []) {
         $res = '' ;
         if ($this->current != null) {
@@ -85,7 +86,7 @@ class BootstrapPanelHelper extends Helper {
             $res .= $this->footer($title, $options) ;
         }
         $res .= '</div>' ;
-		return $res ;
+        return $res ;
     }
 
     protected function _cleanCurrent () {
@@ -108,32 +109,35 @@ class BootstrapPanelHelper extends Helper {
         unset ($options['class']);
         $titleOptions = $this->addClass($titleOptions, 'panel-title');
         return $this->_cleanCurrent().$this->Html->div($class,
-            $this->Html->tag('h3', $title, $titleOptions), 
-            $options
-        ) ; 
+                                                       $this->Html->tag('h3', $title,
+                                                                        $titleOptions),
+                                                       $options
+        ) ;
     }
 
     protected function _createBody ($text, $options = []) {
         $options = $this->addClass($options, 'panel-body');
         $class   = $options['class'];
         unset ($options['class']);
-        return $this->_cleanCurrent().$this->Html->div($class, $text, $options) ; 
+        return $this->_cleanCurrent().$this->Html->div($class, $text, $options) ;
     }
 
     protected function _createFooter ($text = null, $options = []) {
         $options = $this->addClass($options, 'panel-footer');
         $class   = $options['class'];
         unset ($options['class']);
-        return $this->_cleanCurrent().$this->Html->div($class, $text, $options) ; 
+        return $this->_cleanCurrent().$this->Html->div($class, $text, $options) ;
     }
-    
+
     protected function _startPart ($part, $options = []) {
         $res = '' ;
         if ($this->current != null) {
             $res = $this->_endPart () ;
         }
         $this->current = $part ;
-        return $res.$this->Html->div('panel-'.$part.' '.$this->_extractOption('class', $options, ''), null, $options) ;
+        return $res.$this->Html->div('panel-'.$part.' '.$this->_extractOption('class',
+                                                                              $options, ''),
+                                     null, $options) ;
     }
 
     protected function _endPart () {
@@ -142,15 +146,16 @@ class BootstrapPanelHelper extends Helper {
 
     /**
      *
-     * Create / Start the header. If $info is specified as a string, create and return the whole header, otherwize only open the header.
-     * 
+     * Create / Start the header. If $info is specified as a string, create and return the
+     * whole header, otherwize only open the header.
+     *
      * @param array|string $info If string, use as the modal title, otherwize works as $options.
      * @param array $options Options for the header div.
-     * 
+     *
      * Special option (if $info is string):
      *     - close: Add the 'close' button in the header (default true).
      *
-    **/
+     **/
     public function header ($info = null, $options = []) {
         if (is_string($info)) {
             return $this->_createHeader($info, $options) ;
@@ -160,13 +165,14 @@ class BootstrapPanelHelper extends Helper {
 
     /**
      *
-     * Create / Start the body. If $info is not null, it is used as the body content, otherwize start the body div.
-     * 
+     * Create / Start the body. If $info is not null, it is used as the body content, otherwize
+     * start the body div.
+     *
      * @param array|string $info If string, use as the body content, otherwize works as $options.
      * @param array $options Options for the footer div.
-     * 
      *
-    **/
+     *
+     **/
     public function body ($info = null, $options = []) {
         if (is_string($info)) {
             if ($this->current != null) {
@@ -180,15 +186,16 @@ class BootstrapPanelHelper extends Helper {
     protected function _isAssociativeArray ($array) {
         return array_keys($array) !== range(0, count($array) - 1);
     }
-    
+
     /**
      *
-     * Create / Start the footer. If $buttons is specified as an associative arrays or as null, start the footer, otherwize create the footer with the specified text.
-     * 
-     * @param array|string $buttons If string, use as the footer content, if list, concatenate values in the list as content (use for buttons purpose), otherwize works as $options.
+     * Create / Start the footer. If $buttons is specified as an associative arrays or as null,
+     * start the footer, otherwize create the footer with the specified text.
+     *
+     * @param string $text Use as the footer content.
      * @param array $options Options for the footer div.
      *
-    **/
+     **/
     public function footer ($text = "", $options = []) {
         return $this->_createFooter($text, $options) ;
     }
