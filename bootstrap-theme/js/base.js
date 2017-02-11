6199242af04353312b515bee99851dc6f10f4fe6
Holt59 <capelle.mikael@gmail.com>
2017-02-11 22:19:25 +0100

    Update template.

--- bootstrap-theme/js/base.js
+++ bootstrap-theme/js/base.js
@@ -1,13 +1,24 @@
-var sideNavOffset = $('.right-nav').offset().top;
-
-$(window).on('scroll', function (e) {
-    if ($(window).scrollTop() + 20 > sideNavOffset) {
-        $('.right-nav').addClass('right-nav-fixed');
-    }
-    else {
-        $('.right-nav').removeClass('right-nav-fixed');
+$('a[href^="#"]:not([role="tab"])').on('click', function (e) {
+    var section = $($(this).attr('href'));
+    console.log(section);
+    if (section.length > 0) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: section.offset().top - 70
+        }, {}, 500);
     }
 });
+/*
+$(window).on('scroll', function (e) {
+    if ($(window).scrollTop() > topNavOffset) {
+        $('.navbar').addClass('navbar-fixed-top');
+        $('.jumbotron').addClass('more-padding');
+    }
+    else {
+        $('.navbar').removeClass('navbar-fixed-top');
+        $('.jumbotron').removeClass('more-padding');
+    }
+});*/
 
 /* Prevent disabled links from causing a page reload */
 $("li.disabled a").click(function() {
