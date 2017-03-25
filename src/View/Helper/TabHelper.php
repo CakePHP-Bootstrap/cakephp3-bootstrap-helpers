828d02c45ee3787ba5c4255e096374a449d0e0f6
Mikaël Capelle <mikael.capelle@typename.fr>
2017-03-25 13:59:59 +0100

    Start TabHelper.

--- src/View/Helper/TabHelper.php
+++ src/View/Helper/TabHelper.php
@@ -46,9 +46,49 @@ class PanelHelper extends Helper {
      */
     protected $_defaultConfig = [
         'templates' => [
+            'tabStart' => '</div>',
+            'tabHeadingStart' => '<ul class="nav nav-{{type}}{{attrs.class}}" role="tablist"{{attrs>}}',
+            'tabContentStart' => '<div class="tab-content{{attrs.class}}"{{attrs}}>',
+            'tabPanelStart' => '<div role="tabpanel" class="tab-pane{{attrs.class}}" id="{{panelId}}"{{attrs}}>',
+            'tabEnd' => '</div>',
+            'tabHeadingEnd' => '</ul>',
+            'tabContentEnd' => '</div>',
+            'tabPanelEnd' => '</div>'
         ],
         'templateClass' => 'Bootstrap\View\EnhancedStringTemplate'
     ];
+
+    /**
+     * Open a navigation tabs (or pills) with the specified titles.
+     *
+     * ### Options:
+     *
+     * - `type` `'tabs'` or `'pills'`. Default is `'tabs'`.
+     * - `fade` Make tabs fade or not. Default is `true`.
+     * - Other attributes will be added to the wrapping element.
+     *
+     * @param array $title Titles for the navigations tabs / pills.
+     * @param array $options Array of options. See above.
+     *
+     * @return string An HTML string containing opening elements for a
+     * navigation tabs.
+     */
+    public function create($titles, $options = []) {
+        $options += [
+            'type' => 'tabs',
+            'fade' => true,
+            'templateVars' => []
+        ];
+    }
+
+    /**
+     * Closes a navigation tabs, cleans part that have not been closed correctly.
+     *
+     * @return string An HTML string containing closing tags.
+     */
+    public function end() {
+        return $this->_cleanCurrent().$this->formatTemplate('tabEnd', []);
+    }
 
 }
 
