399cc1e865ab907e84d109a4d8e0dd187211e168
Fernando Herrero <ininflamable@gmail.com>
2018-04-29 04:00:25 +0200

    Add possibility to use FontAwesome icons instead of Glyphicons

--- src/View/Helper/HtmlHelper.php
+++ src/View/Helper/HtmlHelper.php
@@ -73,7 +73,7 @@ class HtmlHelper extends \Cake\View\Helper\HtmlHelper {
             'javascriptend' => '</script>',
 
             // New templates for Bootstrap
-            'icon' => '<i aria-hidden="true" class="glyphicon glyphicon-{{type}}{{attrs.class}}"{{attrs}}></i>',
+            'icon' => '<{{tag}} aria-hidden="true" class="{{font}}{{type}}{{attrs.class}}"{{attrs}}></{{tag}}>',
             'label' => '<span class="label label-{{type}}{{attrs.class}}"{{attrs}}>{{content}}</span>',
             'badge' => '<span class="badge{{attrs.class}}"{{attrs}}>{{content}}</span>',
             'alert' => '<div class="alert alert-{{type}}{{attrs.class}}" role="alert"{{attrs}}>{{close}}{{content}}</div>',
@@ -120,18 +120,46 @@ aria-valuenow="{{width}}" aria-valuemin="{{min}}" aria-valuemax="{{max}}" style=
      *
      * @param string $icon Name of the icon.
      * @param array $options Array of options. See above.
+     * @param string $opcions['tag'] Tag use for the icon, "i" for default
+     * @param string $opcions['font'] Font of the icon:
+     *                                "glyphicon" for default Twitter Bootstrap icon.
+     *                                "fa" for Font Awesome icon.
      *
      * @return string The HTML icon.
      */
     public function icon($icon, array $options = []) {
+        $tag = empty($options['tag']) ? 'i' : $options['tag'];
+        $font = 'glyphicon glyphicon-';
+        if (!empty($options['font']) and $options['font'] == 'fa') {
+            $font = 'fa fa-';
+        }
+        unset($options['tag']);
+        unset($options['font']);
+
         $options += [
             'templateVars' => []
         ];
         return $this->formatTemplate('icon', [
+            'tag' => $tag,
+            'font' => $font,
             'type' => $icon,
             'attrs' => $this->templater()->formatAttributes($options),
             'templateVars' => $options['templateVars']
         ]);
+    }
+
+    /**
+     *
+     * Create a Font Awesome icon.
+     *
+     * @param $icon The type of the icon (search, pencil, etc.)
+     * @param $opcions Options for icon
+     * @param $opcions['tag'] Tag use for the icon, "i" for default
+     *
+    **/
+    public function faIcon($icon, array $options = []) {
+        $options['font'] = 'fa';
+        return $this->icon($icon, $options);
     }
 
     /**
